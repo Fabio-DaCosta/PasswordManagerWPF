00cb220e24b4691698bc750c62be4e130c416953
Fabio-DaCosta <fabiojun.dacosta@gmailcom>
2021-06-17 17:58:26 +0200

    Add caliburn.micro package and set it up

---
+++ PasswordManager/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PasswordManager.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword",
+            typeof(string),
+            typeof(PasswordBoxHelper),
+            new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+            if (d is PasswordBox box)
+            {
+                box.PasswordChanged -= PasswordChanged;
+                box.PasswordChanged += PasswordChanged;
+            }
+
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            if (string.Equals(value, GetBoundPassword(d)))
+            {
+                return;
+            }
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        public static void OnBoundPasswordChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is PasswordBox box)) return;
+
+            box.Password = GetBoundPassword(d);
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox password = sender as PasswordBox;
+
+            SetBoundPassword(password, password.Password);
+
+            password.GetType().GetMethod("Select", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(password, new object[] { password.Password.Length, 0 });
+        }
+    }
+}
